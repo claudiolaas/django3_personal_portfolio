fe5e0783e3bc3d4db5f342c99f4a377da7403036
Claudio Laas <claudiolaas@gmail.com>
2020-07-29 17:48:56 -0400

    video 45

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Blog 
 
 # Register your models here.
+admin.site.register(Blog) # tells the site to include 'Models'
--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     
-    path('', views.all_blogs, name = 'all_blogss'),
+    path('', views.all_blogs, name = 'all_blogs'),
 
 ]
