eb7080d0418c7a0b0832d30d52ac533c3363855b
Claudio Laas <claudiolaas@gmail.com>
2020-07-31 14:52:15 -0400

    52 before upload to Python anywhere

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,6 @@ class Blog(models.Model):
     title = models.CharField(max_length= 100)
     description = models.TextField(max_length = 100)
     date = models.DateField()
+
+    def __str__(self):
+        return self.title
